1de6d7e7c9266f840848fcd948a515169f30916f
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-10-28 20:28:49 +0200

    creando spy de useState

--- test/useField.test.js
+++ test/useField.test.js
@@ -1,17 +1,36 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 
 import { useField } from '../hooks/useField'
+import React from 'react'
+
+const useState = vi.fn().mockImplementation((initialValue) => {
+	let value = initialValue
+
+	return [
+		value,
+		(newValue) => {
+			value = newValue
+		},
+	]
+})
+
 
 describe('useField()', () => {
-  it('Shouold be a function', () => {
-    expect(typeof useField).toBe('function')
-  })
+	it('Shouold be a function', () => {
+		expect(typeof useField).toBe('function')
+	})
 
-  it('Should throw if you dont provide any paramater', () => {
-    expect(() => useField()).toThrow()
-  })
+	it('Should throw if you dont provide any paramater', () => {
+		expect(() => useField()).toThrow()
+	})
 
-  it('Should throw a specific error message if not string is provided in "type" parameter', () => {
-    expect(() => useField({type: 2, field: ''})).toThrow(/Type should be a string./)
-  })
+	it('Should throw a specific error message if not string is provided in "type" parameter', () => {
+		expect(() => useField({ type: 2 })).toThrow(/Type should be a string./)
+	})
+
+	it('Should yield an object with the with "type" propierty', () => {
+		const testField = useField({ type: 'text', field: '' })
+
+		expect(testField.value).toBe('text')
+	})
 })
