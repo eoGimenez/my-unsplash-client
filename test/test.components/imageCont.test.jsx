7dedeb4ced91a0ca87970d455e9a7fd48f2cebaf
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-11-08 18:56:01 +0100

    terminando tests de imageCont

--- test/test.components/imageCont.test.jsx
+++ test/test.components/imageCont.test.jsx
@@ -1,5 +1,5 @@
-import { cleanup, render, screen } from '@testing-library/react'
-import { afterEach, describe, it } from 'vitest'
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, it } from 'vitest'
 import ImageCont from '../../components/ImageCont/ImageCont'
 
 const imageTest = { _id: 'testId', label: 'testLabel', imgUrl: 'testImgUrl' }
@@ -17,5 +17,14 @@ describe('Component ImageCont()', () => {
 		setup(imageTest)
 
 		screen.getByText('testLabel')
+	})
+
+	it('Should render DeletePhoto component, when button "delete" is clicked', () => {
+		setup(imageTest)
+
+		const buttonTest = screen.getByText('delete')
+		fireEvent.click(buttonTest)
+
+		expect(screen.getByText('Are you sure ?')).toBeDefined()
 	})
 })
